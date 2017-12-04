ef342daa18609deed0d6b518dc61e831c0cee1c3
unknown <jwilkes1506@gmail.com>
2017-12-04 14:04:37 +0100

    Initial commit, added models

---
+++ src/nl/hu/pafr/ass2/model/Train.java
@@ -0,0 +1,22 @@
+package nl.hu.pafr.ass2.model;
+
+import java.util.ArrayList;
+
+public class Train {
+	
+	// Attributes
+	private String name;
+	private ArrayList<Wagon> wagons = new ArrayList<Wagon>();
+		
+	// Constructor
+	public Train(String name) {				this.name = name;			}
+	
+	// Methods
+	public void addWagon(Wagon wagon) {		this.wagons.add(wagon);		}
+	public void clearWagons() {				wagons.clear(); 	 		}	
+	
+	// Getters
+	public String getName() {				return this.name;			}
+	public ArrayList<Wagon> getWagons() {	return this.wagons; 		}	
+	public boolean hasName() {				return (this.name != "");	}
+}
---
+++ src/nl/hu/pafr/ass2/model/Wagon.java
@@ -0,0 +1,13 @@
+package nl.hu.pafr.ass2.model;
+
+public class Wagon {
+	
+	// Attributes
+	private String name;	
+	
+	// Constructor
+	public Wagon(String name) {		this.name = name;		}	
+
+	// Getters	
+	public String getName() {		return this.name;		}
+}
